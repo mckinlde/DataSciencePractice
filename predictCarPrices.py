e1c34841b172e869992be9bd9ba481c3cc1b868f
dmckinley3 <douglas.e.mckinley@gmail.com>
2022-09-25 16:02:58 -0700

    very light preprocessing; I haven't scaled data yet

--- predictCarPrices.py
+++ predictCarPrices.py
@@ -39,6 +39,7 @@ Columns:
 import pandas as pd
 df = pd.read_csv("/Users/douglasmckinley/Downloads/output.csv")
 print(df.head())
+print("^ read_csv ^")
 
 # Outstanding, we have a dataframe
 
@@ -47,3 +48,48 @@ print(df.head())
 lin_cols = ['Price', 'Year', 'Odo', 'Added', ]
 cat_cols = ['Title', 'Make', 'Model', 'MakeKey', 'ModelKey', 'URL', 'TitleKey', 'Area', 'conditionAttr', 'cylinders', 'drive', 'fuel', 'paint_color', 'size', 'title_status', 'transmission', 'type', 'Body']
 
+# now we want to enumerate our categorical variable labels
+from sklearn import preprocessing
+le = preprocessing.LabelEncoder()
+df[cat_cols] = df[cat_cols].apply(le.fit_transform)
+
+print(df.head())
+print("^ enumerate categorical variables ^")
+
+# Outstanding, now we've got linear values and labeled categories
+
+# Normally we'd scale by either normalizing or standardizing our linear variables here,
+# but I'm interested to see how those affect model performance, so we'll skip it for now
+
+# which means our next step is to split into a train and test set
+from sklearn.model_selection import train_test_split
+
+# I want to be able to tune this, so we'll write a function that parameterizes the split
+def split(df, n):
+    """
+    :param df: dataframe where target variable is final column
+    :param n: % of data to be used as test set, range 0-1
+    :return: X_train,X_test,y_train,y_test
+    """
+    df.iloc
+    X = df.iloc[:,list(range(len(list(df.columns)) - 1))] # X is all columns except last
+    y = df.iloc[:,-1:].values.T # y is everything else, transposed across diagonal
+    y = y[0] # make y 1-dimensional
+    X_train,X_test,y_train,y_test = train_test_split(X,y,train_size = n,test_size = 1-n,random_state = 0)
+    return (X_train,X_test,y_train,y_test)
+
+# Now I can make a train/test in any porportion over a dataframe where the target variable is last
+# I want to predict price, so we'll move that into the last column
+new_cols = [col for col in df.columns if col != 'Price'] + ['Price']
+df = df[new_cols]
+print(df.head())
+print("^ price@end ^")
+
+# and let's run our first pass ambitiously: 90% training
+X_train, X_test, y_train, y_test = split(df, 0.9)
+
+print(X_train.head())
+print(X_test.head())
+print(y_train[0:5])
+print(y_test[0:5])
+print("^ split train test ^")
