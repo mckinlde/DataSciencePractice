584edd6d4df5edc2d842e94faa56a7f3377dd6b0
dmckinley3 <douglas.e.mckinley@gmail.com>
2022-09-28 19:02:43 -0700

    did a sucessful every-column histogramming, recorded impression as comments, and moving on to scatterplot

--- visualizeCarPrices.py
+++ visualizeCarPrices.py
@@ -1,5 +1,5 @@
 # I'm having some trouble training a model, and I'm not sure yet how/if I want to scale features
-
+import matplotlib.pyplot as plt
 # Let's make some graphs to aid in decisionmaking
 
 # First we'll duplicate the ETL tasks done in predictCarPrices.py so far
@@ -42,8 +42,50 @@ df["Price"] = df["Price"].replace("[$,]", "", regex=True).astype(float)
 # First, histogram everything.  I want to check for outliers and get an idea of scale
 
 import matplotlib
+'''
 for (columnName, columnData) in df.iteritems():
     print('Column Name : ', columnName)
     print('Column Contents : ', columnData.values)
 
     columnData.plot.hist(grid=True, bins=20, rwidth=0.9, color='#607c8e')
+    plt.show()
+    input("Any key for next plot")
+    '''
+    # Title looks good, randomly distributed numbers reflect categorical assignment across many strings
+    # Price is clearly goofed.  20 bins and all my values fall in bin 0?  Were bin values not reassigned?
+    # Price Column Contents :  [5650. 1200. 9500. ... 7900. 5900. 7300.]
+    # Make is heavily right skew, with last 4 bins of categories having many members.  Big 4 car companies?
+    # Model similar shape to Make, less drastic
+    # MakeKey "
+    # ModelKey "
+    # Year heavily right skew, logarithmic curve from 1920-2020, with peak at 2005-2010
+    # Odo similar shape to price.
+    # Odo Column Contents :  [169987. 267000.  69600. ... 139000. 135000. 113000.]
+    # Added is bifurcated.  Numerical representations of dates seem to show steady drip of new postings,
+    # with some set of data recorded months previous to majority of data
+    # Added Column Contents :  [738237 738237 738237 ... 738416 738416 738416]
+    # URL is perfect random assortment of categorical-numerical encoding.  Nice.
+    # TitleKey is similar to URL, imperfectly random
+    # Area similar to TitleKey--There are clearly come areas with many more listings
+    # conditionAttr is 1 of 6 buckets, with heavy representation by 0, 2, and 6
+    # cylinders similar to conditionAttr, most common values are 3, 4, 6, and 8
+    # drive is either 0, 1, 2, or 3.  Same to assume many null values exist because scale is much lower.
+    # fuel.  Guess #2 must mean 'gas'.  Does #0 mean diesel?
+    # Paint color is U-shaped.  What color is category 12?
+    # size is either 0, 1, 2, 3, or 4.  Most are 1 or 4.
+    # title_status is almost all 0; goes up to 6
+    # transmission is almost all 0; goes up to 3
+    # type took a while to generate, especially considering it appears to be all 0.  ~900,000 values.
+    # Body is perfect random assortment of categorical-numerical encoding.  Nice.
+
+    # And we're done!  A sucessful Histogramming.  Here are my takeaways:
+    # 1] Listings appear to be uploaded at the same rate.  I have data from a few months, missing a few months,
+    # and then data from a few months more. This could be interesting to create a 'before/after' plot, and may want to
+    # select single ranges for time series predictions.
+    # 2] Something is goofy with the way I'm processing price and odo.  This couldbe a problem with the histogram
+    # plotting, and a scatterplot could tell me more.
+
+# Let's make a scatterplot of odo vs price to see if these values need more cleaning:
+PriceVOdo = df.plot.scatter(x='Price',
+                            y='Odo',
+                            colormap='viridis')
