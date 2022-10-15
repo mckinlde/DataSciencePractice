068736d123c80e6bbb1aa842b1bff2551c71df8b
dmckinley3 <douglas.e.mckinley@gmail.com>
2022-10-15 16:34:23 -0700

    After studying a bunch of snippets, I want to do a deeper dive.  Let's return to car data and make a surface plot

--- visualizeCarPrices.py
+++ visualizeCarPrices.py
@@ -127,3 +127,39 @@ plt.show() # Taking on some tech debt because I'm writing a script when I'd norm
 # and a dense representation of data below the diagonal.
 
 # Let's add these filters to predictCarPrices and see if we can get a functioning linear model.
+
+# We're back after a break--I want to make a surfaceplot because they look cool.  I'll start with copy/paste from:
+# https://matplotlib.org/stable/gallery/mplot3d/surface3d.html
+
+# On initial run, we throw
+# TypeError: 'formatter' must be an instance of matplotlib.ticker.Formatter, not a str
+# Looks like the comment 'A StrMethodFormatter is used automatically' is inaccurate
+
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator
+import numpy as np
+
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+
+# Make data.
+X = np.arange(-5, 5, 0.25)
+Y = np.arange(-5, 5, 0.25)
+X, Y = np.meshgrid(X, Y)
+R = np.sqrt(X**2 + Y**2)
+Z = np.sin(R)
+
+# Plot the surface.
+surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+
+# Customize the z axis.
+ax.set_zlim(-1.01, 1.01)
+ax.zaxis.set_major_locator(LinearLocator(10))
+# A StrMethodFormatter is used automatically
+ax.zaxis.set_major_formatter('{x:.02f}')
+
+# Add a color bar which maps values to colors.
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
+plt.show()
